e73ff772450f2de0a188665f6dcc87551432b404
matheusbourguignon <matheusmbl21@gmail.com>
2024-11-24 16:55:40 -0300

    Configurando variaveis de ambiente

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: 5432,
+  username: process.env.POSTGRES_USER || 'pguser',
+  password: process.env.POSTGRES_PASSWORD ||  'pgpassword',
+  database: process.env.POSTGRES_DATABASE || 'postgres',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
