bb1fc143df013f1ff9190c4fea3edfcccddfd8db
matheusbourguignon <matheusmbl21@gmail.com>
2024-11-28 13:46:06 -0300

    Fazendo relações entre as tabelas checkin, event, user

--- src/checkin/checkin.entity.ts
+++ src/checkin/checkin.entity.ts
@@ -1,4 +1,7 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Unique } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Unique, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from '../user/user.entity';  // Importando a entidade User
+import { Event } from '../event/event.entity';  // Importando a entidade Event
+
 
 @Entity()
 @Unique(['email', 'name']) // Garantindo que o par email + nome seja único para cada check-in
@@ -18,4 +21,14 @@ export class CheckIn {
 
   @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
   updatedAt: Date;
+
+  // Relacionamento com o User (Many-to-One)
+  @ManyToOne(() => User, user => user.checkIns, { nullable: false })
+  @JoinColumn({ name: 'userId' })  // Nome da coluna de referência
+  user: User;
+
+  // Relacionamento com o Event (Many-to-One)
+  @ManyToOne(() => Event, event => event.checkIns, { nullable: false })
+  @JoinColumn({ name: 'eventId' })  // Nome da coluna de referência
+  event: Event;
 }
